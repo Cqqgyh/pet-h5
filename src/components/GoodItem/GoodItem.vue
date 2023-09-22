77a865ec6bda8eda31ae6d44b1489b098831c42d
cqq <945327638@qq.com>
2023-09-22 19:53:08 +0800

    feat: 商品卡片

---
+++ src/components/GoodItem/GoodItem.vue
@@ -0,0 +1,24 @@
+<template>
+  <div
+    class="w-[45vw] bg-[--color-background-3] mt-[10px] pl-[15px] pr-[15px] pt-[5px] pb-[5px] flex flex-col justify-center"
+  >
+    <div class="flex flex-col justify-center items-center">
+      <img
+        src="https://yanxuan-item.nosdn.127.net/63afe2711163b3e4f14857b01f22a603.jpg?type=webp&imageView&quality=65&thumbnail=330x330"
+        alt="产品图片"
+        class="w-[35vw] h-[35vw]"
+      />
+    </div>
+
+    <div class="mt-[5px]">
+      <span class="text-[12px]">100%全麦粉，低脂全麦软欧包 紫薯味 500g</span>
+    </div>
+    <div class="text-[--van-red]">
+      <span class="text-[12px]">¥</span>
+      <span class="text-[20px]">99.00</span>
+    </div>
+  </div>
+</template>
+<script setup lang="ts"></script>
+
+<style scoped lang="less"></style>
